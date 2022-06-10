98c6c0a7a0024c3bdb4c05888ff763d6d7cf3782
Celia Amador <camadorg@redhat.com>
2022-06-10 09:15:47 +0200

    Adding FormKit + Sass support

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="vue-form">
+
+    <FormKit type="form" v-model="formData" :form-class="submitted ? 'hide' : 'show'" submit-label="Register"
+      @submit="submitHandler">
+      <h1>Register!</h1>
+      <p>
+        You can put any type of element inside a form, not just FormKit inputs
+        (although only FormKit inputs are included with the submission).
+      </p>
+      <hr />
+      <FormKit type="text" name="name" label="Your name" placeholder="Jane Doe" help="What do people call you?"
+        validation="required" />
+      <FormKit type="text" name="email" label="Your email" placeholder="jane@example.com"
+        help="What email should we use?" validation="required|email" />
+      <div class="double">
+        <FormKit type="password" name="password" label="Password" validation="required|length:6|matches:/[^a-zA-Z]/"
+          :validation-messages="{
+            matches: 'Please include at least one symbol',
+          }" placeholder="Your password" help="Choose an account password" />
+        <FormKit type="password" name="password_confirm" label="Confirm password" placeholder="Confirm password"
+          validation="required|confirm" help="Choose an account password" />
+      </div>
+    </FormKit>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script lang="ts" setup>
+import { ref } from 'vue'
+const submitted = ref(false)
+const formData = ref({})
+const submitHandler = async () => {
+  // Let's pretend this is an ajax request:
+  await new Promise((r) => setTimeout(r, 1000))
+  submitted.value = true
 }
+</script>
+
+<style lang="scss">
+ .vue-form {
+   min-height: 100vh;
+   display: flex;
+   align-items: center;
+
+   .formkit-label {
+     color: #f00;
+   }
+ }
 </style>
