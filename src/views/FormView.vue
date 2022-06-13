127189eccff01dde851bd51bbed37647ab444493
Celia Amador <camadorg@redhat.com>
2022-06-13 05:49:32 +0200

    Events

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -8,9 +8,9 @@
       <h1>Welcome to Assisted V-UI Form!</h1>
 
       <pf-form>
-        <FormKit type="text" label="Cluster name" name="cluster_name" help="Enter the cluster name"
+        <FormKit type="text" label="Cluster name" @input="test" name="cluster_name" help="Enter the cluster name"
           validation="required" validation-visibility="live" />
-        <FormKit :type="pfTextField" label="Openshift version" name="openshift_version"
+        <FormKit ref="versionInput" label="Openshift version" @input="checkVersion" name="openshift_version"
           help="Enter the Openshift version" validation="required" validation-visibility="live" />
       </pf-form>
 
@@ -26,18 +26,34 @@
 import { createInput } from '@formkit/vue';
 import TextField from '@/components/icons/form-inputs/TextField.vue';
 import { ref } from 'vue'
+
+
 const submitted = ref(false)
 const formData = ref({
   cluster_name: 'My cluster',
   openshift_version: '4.10.3',
 })
+
+const versionInput = ref('4.10.6');
 const pfTextField = createInput(TextField)
+const checkVersion = () => {
+  console.log('vers')
+  versionInput.value.node.input('4.11.0');
+}
+
+const test = () => console.log('hey')
 
 const submitHandler = async () => {
   // Let's pretend this is an ajax request:
   await new Promise((r) => setTimeout(r, 1000))
   submitted.value = true
 }
+
+
+</script>
+
+<script>
+
 </script>
 
 <style lang="scss">
